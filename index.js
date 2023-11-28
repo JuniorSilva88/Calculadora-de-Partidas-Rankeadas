0e2cee23dfa0b49417ef37b1d593e1f40a68dcac
Junior Alexandre da Silva <101154271+JuniorSilva88@users.noreply.github.com>
2023-11-27 21:07:35 -0300

    Edit index.js + print

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-/*Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+function calcularNivel(vitorias, derrotas) {
+    var saldoVitorias = vitorias - derrotas;
+    var nivel = '';
 
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-Saída
+    if (saldoVitorias <= 10) {
+        nivel = 'Ferro';
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = 'Prata';
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = 'Lendário';
+    } else if (saldoVitorias >= 101) {
+        nivel = 'Imortal';
+    }
+    return nivel;
+}
 
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"*/
+var herois = [
+    { nome: "Homem de Ferro", nivel: 10 },
+    { nome: "Batmam", nivel: 15 },
+    { nome: "Flash", nivel: 50 },
+    { nome: "Homem Aranha", nivel: 70 },
+    { nome: "Thor", nivel: 81 },
+    { nome: "Capitão America", nivel: 89 },
+    { nome: "Mulher Maravilha", nivel: 92 },
+    { nome: "Superman", nivel: 10001 }
+];
 
+for (var i = 0; i < herois.length; i++) {
+    var heroi = herois[i];
+    var vitorias = heroi.nivel;
+    var derrotas = 0;
+
+    heroi.nivel = calcularNivel(vitorias, derrotas);
+
+    console.log("O Herói " + heroi.nome + " tem de saldo de : " + vitorias + " está no nível de : " + heroi.nivel);
+}
 
